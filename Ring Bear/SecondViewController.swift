d1a754c6bdb8ad153489483c2f784a010fd209b5
Damian Mazurkiewicz <damian@mazur.io>
2017-09-09 16:30:27 +0100

    T

--- Ring Bear/SecondViewController.swift
+++ Ring Bear/SecondViewController.swift
@@ -12,7 +12,9 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tabBarController?.title = "Test"
+        self.navigationController?.title = "Guest List"
     }
 
     override func didReceiveMemoryWarning() {
