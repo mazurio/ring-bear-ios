13358d9ddcdc3b79a137a307213a22dc39d13ab1
Damian Mazurkiewicz <damian@mazur.io>
2017-10-05 19:58:01 +0100

    Present controllers

--- Ring Bear/SecondViewController.swift
+++ Ring Bear/SecondViewController.swift
@@ -8,20 +8,38 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension SecondViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BasicCell", for: indexPath)
         
-        self.tabBarController?.title = "Test"
-        self.navigationController?.title = "Guest List"
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let destination = storyboard.instantiateViewController(withIdentifier: "ThirdViewNavigationController") as! ThirdViewNavigationController
+        
+        navigationController?.present(destination, animated: true, completion: nil)
+//        navigationController?.pushViewController(destination, animated: true)
+        
     }
-
-
 }
 
---
+++ Ring Bear/ThirdViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdViewController.swift
+//  Ring Bear
+//
+//  Created by Damian Mazurkiewicz on 05/10/2017.
+//  Copyright © 2017 Damian Mazurkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewNavigationController : UINavigationController {}
+
+class ThirdViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
