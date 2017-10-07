aedde364bac3227cec7a76e2177fee71310011ac
Damian Mazurkiewicz <damian@mazur.io>
2017-10-07 13:59:28 +0100

    Refactor

--- Ring Bear/Extensions.swift
+++ Ring Bear/Extensions.swift
@@ -1,14 +1,4 @@
-//
-//  Extensions.swift
-//  Ring Bear
-//
-//  Created by Damian Mazurkiewicz on 07/10/2017.
-//  Copyright © 2017 Damian Mazurkiewicz. All rights reserved.
-//
-
 import Foundation
 import RealmSwift
 
-extension UIViewController {
-
-}
+extension UIViewController {}
--- Ring Bear/Header.swift
+++ Ring Bear/Header.swift
@@ -3,15 +3,7 @@ import UIKit
 class Header: UIView {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var date: UILabel!
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
+
     func update(settings: Settings) {
         var text = ""
         
